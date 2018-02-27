deb02ce13beb446213d73d028a9a78c1bc360217
Amelia Trainer <ameliajo@mit.edu>
2018-02-27 15:47:11 -0500

    scattering angle implementation, and two material properties

--- 2_twoSlabHomogeneousMC/MC_twoSlab.cpp
+++ 2_twoSlabHomogeneousMC/MC_twoSlab.cpp
@@ -11,77 +11,83 @@
 
 
 struct Neutron {
-  float x, phi, mu;
-  Neutron( float x, float phi, float mu ): 
-    x(x), phi(phi), mu(mu) {}
+  float x, mu, phi, w;
+  Neutron( float x, float mu, float phi, float w ): 
+    x(x), mu(mu), phi(phi), w(w) {}
 };
 
 
 int main() {
   float xsMacroTotal1 = 1,   xsMacroAbsor1 = 0.5, 
         xsMacroTotal2 = 1.5, xsMacroAbsor2 = 1.2;
+  float A = 12;
+
 
   std::ofstream myfile;
   myfile.open ("data.txt");
-  int numNeutrons = 1;
+  int numNeutrons = 10;
 
   for ( int i = 0; i < numNeutrons; ++i ){ 
     // Initialize 
     float x   = 2*(float)rand()/RAND_MAX;      // Uniform  0 -> 1
     float mu  = 2*(float)rand()/RAND_MAX-1;    // Uniform -1 -> 1
     float phi = 2*M_PI*(float)rand()/RAND_MAX; // Uniform  0 -> 2pi
+    float w   = 2*(float)rand()/RAND_MAX-1;
      //myfile  << std::setw(15) << std::left << x << 
      //           std::setw(15) << std::left << mu << 
      //           std::setw(15) << std::left << phi << 
      //           std::setw(15) << std::left << phi << std::endl;
  
-    Neutron n(x,mu,phi);
+    Neutron n(x,mu,phi,w);
     while ( true ){
       //std::cout << "sample travel distance" << std::endl;
       float xsi = (float)rand()/RAND_MAX;
       float s = -log(xsi)/xsMacroTotal1;
+      float mu_lab = ( 1 + A*n.mu ) / pow( A*A + 2*A*n.mu + 1, 0.5 );
+      n.w = mu_lab*n.w - pow( 1-mu_lab*mu_lab, 0.5)*pow( 1-n.w*n.w,0.5)*cos(n.phi);
+      if ( n.x < 2 and n.x + n.w*x > 2 ){
+        std::cout << "going from 1 --> 2" << "     " << w << std::endl;
+        n.x = 2+1e-5;
+      }
+      else if ( n.x > 2 and n.x + n.w*x < 2 ){
+        std::cout << "going from 2 --> 1" << "     " << w << std::endl;
+        n.x = 2-1e-5;
+      }
+      else if ( n.x + n.w*x < 0 ){
+        std::cout << "leak below\n" << std::endl;
+        break;
+      }
+      else if ( n.x + n.w*x > 4 ){
+        std::cout << "leak above\n" << std::endl;
+        break;
+      }
+      else {
+        float total = n.x < 2 ? xsMacroTotal1 : xsMacroTotal2;
+        float absor = n.x < 2 ? xsMacroAbsor1 : xsMacroAbsor2;
+        xsi = (float)rand()/RAND_MAX;
+        if ( xsi*total < absor ){
+          if ( n.x < 2 ){ std::cout << "Absorption in 1\n" << std::endl; }
+          else          { std::cout << "Absorption in 2\n" << std::endl; }
+          break;
+        }
+        else {
+          if ( n.x < 2 ){ std::cout << "Scattering in 1" << std::endl; }
+          else          { std::cout << "Scattering in 2" << std::endl; }
 
-      break;
+          n.mu  = 2*(float)rand()/RAND_MAX-1;    // Uniform -1 -> 1
+          n.phi = 2*M_PI*(float)rand()/RAND_MAX; // Uniform  0 -> 2pi
+        }
+
+        n.x += n.w*s;
+
+      }
+      
+      //std::cout << "x position:  " << n.x << std::endl;
+      //myfile << wprime << std::endl;
     } 
   }
   myfile.close();
 
-  /*
-    while ( n.getEnergy() > 1 ){
 
-      // Get the macroscopic cross section of U-238 at this energy
-      sigmaU238_absorption = 5;
-      sigmaU238_elastic = 2;
-      sigmaU238 = sigmaU238_absorption + sigmaU238_elastic;
-      sigmaH1   = 20;
-      NH1 = 10;
-      NU238 = 1;
-      total = sigmaU238 * NU238 + sigmaH1 * NH1;
-      float r1 = (float)rand()/RAND_MAX;
-      if ( r1*total < sigmaU238 * NU238 ){ 
-        float r2 = (float)rand()/RAND_MAX;
-        if ( r2*sigmaU238 < sigmaU238_absorption ){ 
-          numAbsorbed += 1;
-          break;
-        }
-        else {
-          collideNeutronsU238( n ); 
-        }
-      }
-      else {
-        collideNeutronsH1( n );  
-      }
-
-      dataFile2<< n.getEnergy() << std::endl;
-
-    } // while energy is above 10 eV
-  } // for neutron loop
-
-  std::cout << "Num. neutrons absorbed in U-238: " << numAbsorbed << std::endl;
-  std::cout << "%    neutrons absorbed in U-238: " << 100.0*numAbsorbed/numNeutrons << std::endl;
-  dataFile2.close();
-
-
-  */
   return 0;
 }
