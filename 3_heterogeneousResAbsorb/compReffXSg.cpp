3efccd008df15185885d0bd99278a5070b16a7d5
Amelia Trainer <ameliajo@mit.edu>
2018-03-07 18:38:44 -0500

    completed resonance integral calculation for infinite dilution

--- 3_heterogeneousResAbsorb/compReffXSg.cpp
+++ 3_heterogeneousResAbsorb/compReffXSg.cpp
@@ -17,19 +17,9 @@
 
 int main() {
   
-  /*
-  std::ifstream uResData;
-  uResData.open("capture.txt");
-  std::ifstream is("capture.txt");
-  std::istream_iterator<float> start(is), end;
-  std::vector<float> uData(start, end);
-  std::vector<float>::const_iterator beginning = uData.begin();
-  std::vector<float> energyList (beginning,      beginning+1000),
-                     elasticList(beginning+1000, beginning+2000),
-                     gammaList  (beginning+2000, beginning+3000);
-                     */
 
-  std::vector<double> energy(10000), capture300(10000), capture1000(10000);
+  int vecSize = 5000;
+  std::vector<double> energy(vecSize), capture300(vecSize), capture1000(vecSize);
   std::ifstream in("capture.txt");
   int i = 0;
   while(!in.eof()){
@@ -40,31 +30,27 @@ int main() {
   }
 
   // i = 0 corresponds 
-
-  double integral_sigma_flux_6_to_10  = 0, integral_sigma_flux_10_to_25 = 0,
-         integral_sigma_flux_25_to_50 = 0;
-  double resonance_integral_6_to_10 = 0, resonance_integral_10_to_25 = 0,
-         resonance_integral_25_to_50 = 0;
-  for ( int i = 1; i < 10000; ++i ){
+  std::vector<double> integral_sigma_flux { 0, 0, 0 };
+  std::vector<double> resonance_integral{ 0, 0, 0 };
+  for ( int i = 1; i < vecSize; ++i ){
     // now we're going to integrate from eLeft --> eRight
-    double eLeft  = energy[i-1];
-    double eRight = energy[i];
-    double xs = ( capture300[i-1] + capture300[i] ) / 2.0;
-    double toAdd = xs * log( eRight / eLeft );
-    if      ( eLeft <= 10 ){ integral_sigma_flux_6_to_10  += toAdd; }
-    else if ( eLeft <= 25 ){ integral_sigma_flux_10_to_25 += toAdd; }
-    else                   { integral_sigma_flux_25_to_50 += toAdd; }
+    double eLeft  = energy[i-1], 
+           eRight = energy[i],
+           xs     = ( capture300[i-1] + capture300[i] ) / 2.0,
+           toAdd  = xs * log( eRight / eLeft );
+    if      ( eLeft <= 10 ){ integral_sigma_flux[0] += toAdd; }
+    else if ( eLeft <= 25 ){ integral_sigma_flux[1] += toAdd; }
+    else                   { integral_sigma_flux[2] += toAdd; }
 
     toAdd = log( eRight / eLeft );
-    if      ( eLeft <= 10 ){ resonance_integral_6_to_10  += toAdd; }
-    else if ( eLeft <= 25 ){ resonance_integral_10_to_25 += toAdd; }
-    else                   { resonance_integral_25_to_50 += toAdd; }
-
+    if      ( eLeft <= 10 ){ resonance_integral[0] += toAdd; }
+    else if ( eLeft <= 25 ){ resonance_integral[1] += toAdd; }
+    else                   { resonance_integral[2] += toAdd; }
 
   } 
-  std::cout << resonance_integral_6_to_10 / integral_sigma_flux_6_to_10 << std::endl;
-  std::cout << integral_sigma_flux_10_to_25 << std::endl;
-  std::cout << integral_sigma_flux_25_to_50 << std::endl;
+  std::cout << "---------  " << resonance_integral[0] << "     " << resonance_integral[0] / integral_sigma_flux[0]<< std::endl;
+  std::cout << "---------  " << resonance_integral[1] << "     " << resonance_integral[0] / integral_sigma_flux[1]<< std::endl;
+  std::cout << "---------  " << resonance_integral[2] << "     " << resonance_integral[0] / integral_sigma_flux[2]<< std::endl;
   return 0;
 
 }
