02b056b7fa8b0d400cc2d7a0b482b3c9d9273aeb
Amelia Trainer <ameliajo@mit.edu>
2018-03-08 08:43:56 -0500

    added wide and intermediate approximations

--- 3_heterogeneousResAbsorb/compReffXSg.cpp
+++ 3_heterogeneousResAbsorb/compReffXSg.cpp
@@ -19,11 +19,25 @@ auto narrowResonanceFlux( double sigmaTotU238, double sigmaDilution ){
   return ( sigmaPotU238 + sigmaDilution ) / ( sigmaTotU238 + sigmaDilution );
 }
 
+auto wideResonanceFlux( double sigmaAbsU238, double sigmaDilution ){
+  return ( sigmaDilution ) / ( sigmaAbsU238 + sigmaDilution );
+}
+
+auto intermediateResonanceFlux( double sigmaAbsU238, double sigmaDilution ){
+  double lambda = 0.5;
+  double sigmaPotU238 = 11.2934;
+  return ( lambda * sigmaPotU238 + sigmaDilution ) / 
+         ( sigmaAbsU238 +  lambda * sigmaPotU238 + sigmaDilution );
+}
+
+
+
 
   
 auto calcResIntegralXS(){
   bool infiniteDilution = false;
-  bool narrowResonance = true;
+  bool narrowResonance = false;
+  bool wideResonance = true;
 
   int vecSize = 5000;
   std::vector<double> energy(vecSize), capture300(vecSize), capture1000(vecSize), total300(vecSize), total1000(vecSize);
@@ -70,23 +84,91 @@ auto calcResIntegralXS(){
       // now we're going to integrate from eLeft --> eRight
       double eLeft  = energy[i-1], 
              eRight = energy[i],
-             sigmaTotU238_300 = ( total300[i-1]   + total300[i]   ) / 2.0,
-             sigmaAbsU238_300 = ( capture300[i-1] + capture300[i] ) / 2.0,
+             sigmaTotU238_300  = ( total300[i-1]    + total300[i]   ) / 2.0,
+             sigmaTotU238_1000 = ( total1000[i-1]   + total1000[i]   ) / 2.0,
+             sigmaAbsU238_300  = ( capture300[i-1]  + capture300[i] ) / 2.0,
+             sigmaAbsU238_1000 = ( capture1000[i-1] + capture1000[i] ) / 2.0,
              sigmaDilution = 2000;
 
-      double flux = narrowResonanceFlux( sigmaTotU238_300, sigmaDilution ); 
-      double toAdd  = flux * sigmaAbsU238_300 * log( eRight / eLeft );
-      if      ( eLeft <= 10 ){ integral_sigma_flux_300[0] += toAdd; }
-      else if ( eLeft <= 25 ){ integral_sigma_flux_300[1] += toAdd; }
-      else                   { integral_sigma_flux_300[2] += toAdd; }
- 
-      toAdd = flux * log( eRight / eLeft );
-      if      ( eLeft <= 10 ){ resonance_integral_300[0] += toAdd; }
-      else if ( eLeft <= 25 ){ resonance_integral_300[1] += toAdd; }
-      else                   { resonance_integral_300[2] += toAdd; }
+      double flux300  = narrowResonanceFlux( sigmaTotU238_300,  sigmaDilution ); 
+      double flux1000 = narrowResonanceFlux( sigmaTotU238_1000, sigmaDilution ); 
+
+      double intPieceXS300   = flux300  * sigmaAbsU238_300  * log( eRight / eLeft ),
+             intPieceXS1000  = flux1000 * sigmaAbsU238_1000 * log( eRight / eLeft ),
+             resIntPiece300  = flux300  * log( eRight / eLeft ),
+             resIntPiece1000 = flux1000 * log( eRight / eLeft );
+      int index;
+      if      ( eLeft <= 10 ){ index = 0; } 
+      else if ( eLeft <= 25 ){ index = 1; }
+      else                   { index = 2; }
+      integral_sigma_flux_300[index]  += intPieceXS300; 
+      integral_sigma_flux_1000[index] += intPieceXS1000; 
+      resonance_integral_300[index]   += resIntPiece300; 
+      resonance_integral_1000[index]  += resIntPiece1000; 
 
 
     }
+    else if ( wideResonance ){
+      // THIS IS FOR WIDE RESONANCE APPROXIMATION
+      // now we're going to integrate from eLeft --> eRight
+      double eLeft  = energy[i-1], 
+             eRight = energy[i],
+             sigmaTotU238_300  = ( total300[i-1]    + total300[i]   ) / 2.0,
+             sigmaTotU238_1000 = ( total1000[i-1]   + total1000[i]   ) / 2.0,
+             sigmaAbsU238_300  = ( capture300[i-1]  + capture300[i] ) / 2.0,
+             sigmaAbsU238_1000 = ( capture1000[i-1] + capture1000[i] ) / 2.0,
+             sigmaDilution = 2000;
+
+      double flux300  = wideResonanceFlux( sigmaAbsU238_300,  sigmaDilution ); 
+      double flux1000 = wideResonanceFlux( sigmaAbsU238_1000, sigmaDilution ); 
+
+      double intPieceXS300   = flux300  * sigmaAbsU238_300  * log( eRight / eLeft ),
+             intPieceXS1000  = flux1000 * sigmaAbsU238_1000 * log( eRight / eLeft ),
+             resIntPiece300  = flux300  * log( eRight / eLeft ),
+             resIntPiece1000 = flux1000 * log( eRight / eLeft );
+      int index;
+      if      ( eLeft <= 10 ){ index = 0; } 
+      else if ( eLeft <= 25 ){ index = 1; }
+      else                   { index = 2; }
+      integral_sigma_flux_300[index]  += intPieceXS300; 
+      integral_sigma_flux_1000[index] += intPieceXS1000; 
+      resonance_integral_300[index]   += resIntPiece300; 
+      resonance_integral_1000[index]  += resIntPiece1000; 
+
+
+    }
+    else {
+      // THIS IS FOR INTERMEDIATE RESONANCE APPROXIMATION
+      // now we're going to integrate from eLeft --> eRight
+      double eLeft  = energy[i-1], 
+             eRight = energy[i],
+             sigmaTotU238_300  = ( total300[i-1]    + total300[i]   ) / 2.0,
+             sigmaTotU238_1000 = ( total1000[i-1]   + total1000[i]   ) / 2.0,
+             sigmaAbsU238_300  = ( capture300[i-1]  + capture300[i] ) / 2.0,
+             sigmaAbsU238_1000 = ( capture1000[i-1] + capture1000[i] ) / 2.0,
+             sigmaDilution = 2000;
+
+      double flux300  = intermediateResonanceFlux( sigmaAbsU238_300,  sigmaDilution ); 
+      double flux1000 = intermediateResonanceFlux( sigmaAbsU238_1000, sigmaDilution ); 
+
+      double intPieceXS300   = flux300  * sigmaAbsU238_300  * log( eRight / eLeft ),
+             intPieceXS1000  = flux1000 * sigmaAbsU238_1000 * log( eRight / eLeft ),
+             resIntPiece300  = flux300  * log( eRight / eLeft ),
+             resIntPiece1000 = flux1000 * log( eRight / eLeft );
+      int index;
+      if      ( eLeft <= 10 ){ index = 0; } 
+      else if ( eLeft <= 25 ){ index = 1; }
+      else                   { index = 2; }
+      integral_sigma_flux_300[index]  += intPieceXS300; 
+      integral_sigma_flux_1000[index] += intPieceXS1000; 
+      resonance_integral_300[index]   += resIntPiece300; 
+      resonance_integral_1000[index]  += resIntPiece1000; 
+
+
+    }
+
+
+
 
   } 
   std::cout << "---------  " << resonance_integral_300[0] << "     " << resonance_integral_300[0] / integral_sigma_flux_300[0]<< std::endl;
