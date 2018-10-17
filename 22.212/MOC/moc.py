99034c526402534f136b20700b6780e30d670479
Amelia Trainer <ameliajo@mit.edu>
2018-10-17 18:28:47 -0400

    more comments and cleaning

--- 22.212/MOC/moc.py
+++ 22.212/MOC/moc.py
@@ -18,54 +18,71 @@ class simulation():
         self.phi = [[0.0]*len(q[0]) for temp in q]
         self.q   = [qRow[:]         for qRow in q]
 
-
 class ray:
-    def __init__(self,x,y,polar,cos_azimuthal,length):
-        self.x0 = x; self.y0 = y; self.l = length; 
-        self.cos_azimuthal = cos_azimuthal
-
-        self.sin = cos_azimuthal*np.sin(polar)
-        self.cos = cos_azimuthal*np.cos(polar)
+    def __init__(self,x,y,polar,cos_azi,length):
+        self.x0 = x; self.y0 = y; self.l = length; self.psi = None
+        self.cos_azi = cos_azi
+        sin_azi = np.sin(np.arccos(cos_azi))
+        self.sin, self.cos = sin_azi*np.sin(polar), sin_azi*np.cos(polar)
 
 def getCell(cells,r):
+    """
+    Since we're working with a 3x3 lattice, we need to find which of these 9 we
+    are in. 
+    Input:  Vector filled with cell objects, and our ray object
+    Output: Correct cell object, List of X-Planes, List of Y-Planes, and list 
+            of circles in the cell 
+    """
+
     cell, counter = None, 0
     while not cell:
-        cell = whichCellAmIIn(r,cells)
-        if not cell:
-            r.x0 += (1.0e-5*r.cos)*(-1)**counter*(1+counter)
-            r.y0 += (1.0e-5*r.sin)*(-1)**counter*(1+counter)
-            counter += 1
+        #cell = whichCellAmIIn(r,cells)
+        for cell in cells:
+            if r.x0 >= cell.L.x and r.x0 <= cell.R.x and r.y0 >= cell.D.y and r.y0 <= cell.U.y:
+                return cell, [cell.L,cell.R], [cell.U,cell.D], cell.C
 
-    xPlanes, yPlanes = [cell.L,cell.R], [cell.U,cell.D]
-    circles = cell.C
-    return cell,xPlanes,yPlanes,circles
+        smudge = 1.0e-5 * (-1)**counter * (1+counter)
+        r.x0, r.y0 = r.x0+smudge*r.cos, r.y0+smudge*r.sin
+        counter += 1
 
-
-def whichCellAmIIn(r,cells):
-    for cell in cells:
-        if r.x0 >= cell.L.x and r.x0 <= cell.R.x and r.y0 >= cell.D.y and r.y0 <= cell.U.y:
-            return cell
-    return None
 
 
 def doesItCrossCircle(c,intersections,r):
-    A = r.sin*r.sin+r.cos*r.cos
-    B = 2.0*(r.x0*r.cos - c.x*r.cos + r.y0*r.sin - c.y*r.sin)
-    C = r.x0**2 - 2.0*r.x0*c.x + c.x*c.x + r.y0**2 -2.0*r.y0*c.y + c.y**2 - c.r*c.r
+    """
+    Update the intersections vector to account for whether the ray passes 
+    through given circle c. I want to see if my ray is going to cross the circle 
+    in question. So I take (x-xc)^2 + (y-yc)^2 = r^2, parameterize 
+    x = x0 + t*cos, y = y0 + t*sin, and solving for t will require solving the 
+    quadratic formula below. If t is  real and positive, then it's in our path 
+    and we should add it to the intersections list.
+
+    Inputs: circle object, existing list of intersections, ray object
+    Output: amended list of intersections
+    """
+    A = r.sin**2 + r.cos**2
+    B = 2*(r.x0*r.cos - c.x*r.cos + r.y0*r.sin - c.y*r.sin)
+    C = r.x0**2 - 2*r.x0*c.x + c.x*c.x + r.y0**2 - 2*r.y0*c.y + c.y**2 - c.r**2
 
     for pm in [1.0,-1.0]:
-        if (B*B-4*A*C < 0): continue
-        t = (-B + pm*(B*B-4*A*C)**0.5)/(2.0*A)
+        if B*B - 4*A*C < 0: continue
+        t = (-B + pm*(B*B-4*A*C)**0.5)/(2*A)
         if (abs(t.imag) < 1.0e-20 and t.real > 0.0):
-            t = t.real
-            xInt, yInt = r.x0 + t*r.cos, r.y0 + t*r.sin
-            intersections.append({"xInt":xInt,"yInt":yInt,"t":t,
-                                  "bc":None,"dir":None})
+            t, xInt, yInt = t.real, r.x0 + t*r.cos, r.y0 + t*r.sin
+            intersections.append({"x":xInt,"y":yInt,"t":t,"bc":None,"dir":None})
     return intersections
 
 
 
 def findAllIntersections(xPlanes,yPlanes,circles,r):
+    """
+    Find all potential intersections that ray r will go through. Consider three
+    cases: ray is outside a circle, but inside cell, ray is in a circle but not
+    the innermost circle, and ray is in the innermost circle. 1st case means 
+    that we are only interested in wall intersections and intersection with the
+    largest circle. 2nd case means we're interested in the boundary of our 
+    current circle, and the boundary of the next smaller ring. 3rd case means
+    we only need to check the boundary of our current circle.
+    """
     intersections = []
     whereRayIs = whichCircleIsRayIn(r,circles)
     if not whereRayIs:
@@ -73,13 +90,13 @@ def findAllIntersections(xPlanes,yPlanes,circles,r):
             t = (xPlane.x - r.x0)/r.cos
             if t <= 0: continue
             xInt, yInt = round(r.x0+t*r.cos,10), round(r.y0+t*r.sin,10)
-            intersections.append({"xInt":xInt,"yInt":yInt,"t":t,"bc":xPlane.bc,"dir":"x"})
+            intersections.append({"x":xInt,"y":yInt,"t":t,"bc":xPlane.bc,"dir":"x"})
 
         for yPlane in yPlanes:
             t = (yPlane.y - r.y0)/r.sin
             if t <= 0: continue
             xInt, yInt = round(r.x0+t*r.cos,10), round(r.y0+t*r.sin,10)
-            intersections.append({"xInt":xInt,"yInt":yInt,"t":t,"bc":yPlane.bc,"dir":"y"})
+            intersections.append({"x":xInt,"y":yInt,"t":t,"bc":yPlane.bc,"dir":"y"})
 
         return doesItCrossCircle(circles[0],intersections,r)
     
@@ -95,62 +112,58 @@ def findAllIntersections(xPlanes,yPlanes,circles,r):
 
 
 def findFirstIntersection(xPlanes,yPlanes,circles,r,recentDist):
+    """
+    Find the first intersection that ray r is going to encounter. Uses the 
+    function ``findAllIntersections'' to get all possible/logical candidates.
+    We look at the intersection with the minimum t (parametrization variable),
+    which should indicate which it hits first.
+    """
     intersections = findAllIntersections(xPlanes,yPlanes,circles,r)
     if intersections == []: print("NO MORE INTERSECTIONS"); return None
-    tMin = intersections[0]["t"]
-    firstInt = intersections[0]
-    for intersection in intersections:
-        t = intersection["t"]
-        if ( t < tMin ):
-            firstInt = intersection
-            tMin = t
-
-    r.x1, r.y1 = firstInt["xInt"], firstInt["yInt"]
-    dist_traveled = ((r.x1-r.x0)**2+(r.y1-r.y0)**2)**0.5/r.cos_azimuthal
-    firstInt["t"], firstInt["dist to get to x1y1"] = tMin, dist_traveled
-
+    times = [i["t"] for i in intersections]
+    firstInt = intersections[times.index(min(times))]
+    r.x1, r.y1 = firstInt["x"], firstInt["y"]
+    firstInt["dist"] = ((r.x1-r.x0)**2+(r.y1-r.y0)**2)**0.5/r.cos_azi
     if weAreStuck(recentDist,firstInt): print("GOT STUCK"); return None
     return firstInt,recentDist
  
 
 
 def updateRay(r,intersection):
+    """
+    Update ray by moving it to the next boundary, and potentially changing its
+    direction (if we hit a reflective boundary).
+    """
     if intersection["bc"] == "ref":     # Do we bounce off?
         if   intersection["dir"] == "x": r.cos = -r.cos
         elif intersection["dir"] == "y": r.sin = -r.sin
    
-    r.x0, r.y0 = r.x1+1.0e-7*r.cos, r.y1+1.0e-7*r.sin
-    r.l = r.l - intersection["dist to get to x1y1"]
+    r.x0, r.y0 = r.x1 + 1e-7*r.cos, r.y1 + 1e-7*r.sin
+    r.l -= intersection["dist"]
 
 
 
 def whichCircleIsRayIn(r,circles):
-    potential_circles = [c for c in circles if isRayInCircle(r,c)]
-    if potential_circles == []: return None
-    radiusList = [c.r for c in potential_circles]
-    return potential_circles[radiusList.index(min(radiusList))]
+    outsideRings = [c for c in circles if (r.x0-c.x)**2+(r.y0-c.y)**2 <= c.r**2]
+    if outsideRings == []: return None
+    radiusList = [c.r for c in outsideRings]
+    return outsideRings[radiusList.index(min(radiusList))]
 
 
-def isRayInCircle(r,circle):
-    return (r.x0-circle.x)**2+(r.y0-circle.y)**2 <= (circle.r)**2
-
-
-def weAreStuck(recentDist,firstIntersection):
+def weAreStuck(recentDist,firstIntersect):
     if len(recentDist) < 5 : return False
     recentDist.pop(0)
-    recentDist.append(firstIntersection["dist to get to x1y1"])
+    recentDist.append(firstIntersect["dist"])
     return True if (sum(recentDist) < 1e-8) else False
 
 
-
-def runRays(sim,numRaysPerRun,firstIteration):
+def runRays(sim,numRaysPerRun,firstIteration=False):
     distInFuel, distInMod = 0.0, 0.0
     for ray in range(numRaysPerRun):
-        if firstIteration:
-            rayTracks.append([["RayNumber"+str(ray)]])
+        if firstIteration: rayTracks.append([["RayNumber"+str(ray)]])
         dFuel,dMod = runRay(sim,ray,firstIteration)
         if firstIteration:
-            distInFuel, distInMod = distInFuel + dFuel, distInMod + dMod
+            distInFuel += dFuel; distInMod  += dMod; 
 
     return distInMod,distInFuel     
 
@@ -159,22 +172,17 @@ def runRay(sim,rayNum,firstIteration):
 
     if firstIteration:
         # Initialize Ray 
+        
         x0 = (cells[2].R.x-cells[0].L.x)*(random.random()) + cells[0].L.x
         y0 = (cells[8].U.y-cells[0].D.y)*(random.random()) + cells[0].D.y
         polar = 2.0*pi*random.random()
-        azimuthal = pi*random.random()-(pi*0.5)
+        cos_azimuthal = random.random()
         
-        r = ray(x0,y0,polar,np.cos(azimuthal),10.0) # should be 3m length
+        r = ray(x0,y0,polar,cos_azimuthal,10.0) # should be 3m length
 
         cell,xPlanes,yPlanes,circles = getCell(cells,r)
 
-        # figure out where x0 y0 is, then make psi = q[that]/sigmaT
-        whereRayIs = whichCircleIsRayIn(r,circles)
-        if not whereRayIs: r.psi = sim.q[cell.id][0]/(4.0*pi*cell.mat.SigmaT)
-        else             : r.psi = sim.q[cell.id][1]/(4.0*pi*cell.C[0].mat.SigmaT)
 
-        #print("found where ray is, going into loop")
-        rayTracks[rayNum][0].append(["initial whereRayIs",whereRayIs])
         recentDist = [] 
         distInFuel,distInMod = 0.0, 0.0
         while(r.l > 0):
@@ -183,59 +191,59 @@ def runRay(sim,rayNum,firstIteration):
             
             r.xMax, r.yMax = r.x0+r.cos*r.l, r.y0+r.sin*r.l
 
-            firstIntersection,recentDist = findFirstIntersection(xPlanes,yPlanes,circles,r,recentDist)
-            if not firstIntersection: print('something bad'); return
+            firstIntersect,recentDist = findFirstIntersection(xPlanes,yPlanes,circles,r,recentDist)
+            if not firstIntersect: print('something bad'); return distInFuel,distInMod
 
             whereRayIs = whichCircleIsRayIn(r,circles)
             if whereRayIs: idVal = whereRayIs.id
-            if not whereRayIs: idVal = 0; whereRayIs = cell; 
+            else:          idVal = 0; whereRayIs = cell; 
             mat = whereRayIs.mat
+            if not r.psi: 
+                r.psi = sim.q[cell.id][idVal]/(4.0*pi*mat.SigT)
+                rayTracks[rayNum][0].append(["initial whereRayIs",whereRayIs])
+
              
-            if (firstIntersection["t"] > r.l): r.x = r.xMax; r.y = r.yMax; break
+            if (firstIntersect["t"] > r.l): r.x = r.xMax; r.y = r.yMax; break
     
-            currentCircle = whichCircleIsRayIn(r,circles)
-            if (whereRayIs): plotRaySegment(r,whereRayIs.color,firstIntersection)
-            if (currentCircle): plotRaySegment(r,currentCircle.color,firstIntersection)
-            else: plotRaySegment(r,cell.color,firstIntersection)
+            #if (whereRayIs): plotRaySegment(r,whereRayIs.color,firstIntersect)
 
-            updateRay(r,firstIntersection)
+            updateRay(r,firstIntersect)
 
-            length = firstIntersection["dist to get to x1y1"]#/np.cos(r.azimuthal)
-
-            rayTracks[rayNum].append({"length":length,"mat":mat,"idVal":idVal,"l":r.l,"cellID":cell.id})
+            rayTracks[rayNum].append({"length":firstIntersect["dist"],"mat":mat,
+                                      "idVal":idVal,"l":r.l,"cellID":cell.id})
         
-            deltaPsi = (r.psi - (sim.q[cell.id][idVal]/(4.0*pi*mat.SigmaT)))*(1.0-exp(-mat.SigmaT*length))
+            deltaPsi = (r.psi - (sim.q[cell.id][idVal]/(4.0*pi*mat.SigT))) *  \
+                       (1.0 - exp(-mat.SigT*firstIntersect["dist"]))
+
             r.psi -= deltaPsi
             if ( r.l < 250.0 ):
                 sim.phi[cell.id][idVal] += 4.0*pi*deltaPsi
-                if whereRayIs.id == 1:
-                    distInFuel += firstIntersection["dist to get to x1y1"]
-                else:
-                    distInMod += firstIntersection["dist to get to x1y1"]
+                if whereRayIs.id == 1: distInFuel += firstIntersect["dist"]
+                else:                  distInMod  += firstIntersect["dist"]
 
 
-        rayTracks[rayNum][0].append(["distInFuel",distInFuel])
-        rayTracks[rayNum][0].append(["distInMod", distInMod] )
+        rayTracks[rayNum][0] += [["distInFuel",distInFuel],["distInMod",distInMod]]
+
         return distInFuel,distInMod
 
     else:
         tracks = rayTracks[rayNum]
-        mat = rayTracks[0][2]["mat"]
-        cellID = rayTracks[0][2]["cellID"]
+        mat, cellId = rayTracks[0][2]["mat"], rayTracks[0][2]["cellID"]
         whereRayIs = tracks[0][1]
-        psi = 0.0;
-        if not whereRayIs: psi = sim.q[cellID][0]/(4.0*pi*mat.SigmaT)
-        else             : psi = sim.q[cellID][1]/(4.0*pi*mat.SigmaT)
+        psi = sim.q[cellId][1]/(4.0*pi*mat.SigT) if whereRayIs else         \
+              sim.q[cellId][0]/(4.0*pi*mat.SigT)
+
         for i in range(len(tracks)-1):
             mat    = tracks[i+1]["mat"   ]; idVal  = tracks[i+1]["idVal"]
-            length = tracks[i+1]["length"]; l      = tracks[i+1]["l"]
+            length = tracks[i+1]["length"]; l      = tracks[i+1]["l"    ]
             cellID = tracks[i+1]["cellID"]; 
-            deltaPsi = (psi - (sim.q[cellID][idVal]/(4.0*pi*mat.SigmaT)))*(1.0-exp(-mat.SigmaT*length))
+            deltaPsi = (psi - (sim.q[cellID][idVal]/(4.0*pi*mat.SigT)))*    \
+                       (1.0 - exp(-mat.SigT*length))
             psi  -= deltaPsi
             if ( l < 250.0 ):
                 sim.phi[cellID][idVal] += 4.0*pi*deltaPsi
-        distInFuel = tracks[0][2][1]
-        distInMod  = tracks[0][3][1]
+
+        distInFuel, distInMod = tracks[0][2][1], tracks[0][3][1]
         return distInFuel,distInMod
  
     
@@ -275,7 +283,7 @@ for y in range(3):
         counter += 1
         cells.append(cell)
 
-allRegionsCell1 = [cells[0]]+cells[0].C
+allRegInCell = [cells[0]]+cells[0].C
 numRegionsPerCell = 1 + len(radii)
 
 volumes = getVolumes(numRegionsPerCell,cells[0].C,sideLength)
@@ -292,39 +300,39 @@ for i in range(len(cells)):
 
 for c in range(len(cells)):
     for i in range(numRegionsPerCell):
-        mat = allRegionsCell1[i].mat
-        q_guess[c][i] = ( mat.SigmaS + mat.SigmaF )
-        oldFissionSource[c][i] = mat.SigmaF
+        mat = allRegInCell[i].mat
+        q_guess[c][i] = ( mat.SigS + mat.SigF )
+        oldFissionSource[c][i] = mat.SigF
+
+
 
 converged = False
 firstIteration = True
-numRaysPerRun = 10
+numRaysPerRun = 100
 
 random.seed(1)
 counter = 0
+inv_trackLength = 0.0
 kVals = []
 sim = simulation(q_guess,k_guess) 
 while not converged:
-    sim = simulation(sim.q,sim.k) 
-    if firstIteration:
-        distInMod,distInFuel = runRays(sim,numRaysPerRun,firstIteration)
-    else:
-        runRays(sim,numRaysPerRun,firstIteration)
 
-    inv_trackLength = 1.0/(distInMod+distInFuel)
+    if counter == 0: 
+        distInMod,distInFuel = runRays(sim,numRaysPerRun,True)
+        inv_trackLength = 1.0/(distInMod+distInFuel)
+    else:                      runRays(sim,numRaysPerRun)
 
-    for cell in cells:
-        sim.phi[cell.id] = [inv_trackLength * x for x in sim.phi[cell.id]]
+    sim.phi = [[inv_trackLength*x for x in sim_phi_cell] for sim_phi_cell in sim.phi]
 
 
     # Update phi
     for cell in cells:
         for i in range(numRegionsPerCell):
-            mat = allRegionsCell1[i].mat
-            idVal = allRegionsCell1[i].id
-            sim.phi[cell.id][i] = sim.phi[cell.id][i]/(mat.SigmaT*volumes[idVal]) + sim.q[cell.id][i]/mat.SigmaT
-            sim.q[cell.id][i] = ( mat.SigmaS*sim.phi[cell.id][i] + mat.SigmaF*sim.phi[cell.id][i] )
-            newFissionSource[cell.id][i] = mat.SigmaF*sim.phi[cell.id][i]
+            mat, V = allRegInCell[i].mat, volumes[allRegInCell[i].id]
+            thisPhi, thisQ = sim.phi[cell.id][i], sim.q[cell.id][i]
+            sim.phi[cell.id][i] = sim.phi[cell.id][i]/(mat.SigT*V) + sim.q[cell.id][i]/mat.SigT
+            sim.q[cell.id][i] = mat.SigS*sim.phi[cell.id][i] + mat.SigF*sim.phi[cell.id][i]
+            newFissionSource[cell.id][i] = mat.SigF*sim.phi[cell.id][i]
 
     kNumer, kDenom = 0.0, 0.0
     for i in range(len(newFissionSource)):
