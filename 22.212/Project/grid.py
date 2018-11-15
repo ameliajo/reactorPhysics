56337b4d519f8a243345bbaa87dbc57201ee568f
Amelia Trainer <ameliajo@mit.edu>
2018-11-15 13:05:37 -0500

    saving nao bc want to make mod cells different and seems spooky

--- 22.212/Project/grid.py
+++ 22.212/Project/grid.py
@@ -11,114 +11,61 @@ uo2.add_element('U', 1.0, enrichment=3.0)
 uo2.add_element('O', 2.0)
 uo2.set_density('g/cc', 10.0)
 
-
-#zirconium = openmc.Material(2, "zirconium")
-#zirconium.add_element('Zr', 1.0)
-#zirconium.set_density('g/cm3', 6.6)
-
 water = openmc.Material(3, "h2o")
-water.add_nuclide('H1', 2.0)
-water.add_nuclide('O16', 1.0)
+water.add_element('H', 2.0)
+water.add_element('O', 1.0)
 water.set_density('g/cm3', 1.0)
 
-mats = openmc.Materials([uo2, zirconium, water])
-
-
+mats = openmc.Materials([uo2, water])
 
 mats.export_to_xml()
-
 
 
 ##################################################################
 # GEOMETRY
 ##################################################################
-sideLen = 1.26
-pitch = sideLen*3
+L = 1.26
+pitch = L*3
 
-fuel_or_vec = []
-clad_ir_vec = []
-clad_or_vec = []
-for i in range(3):
-    for j in range(3):
-        fuel_or_vec.append(openmc.ZCylinder(R=0.39,x0=(1+2*i)*sideLen/2,y0=(1+2*j)*sideLen/2))
-#        clad_ir_vec.append(openmc.ZCylinder(R=0.40,x0=(1+2*i)*sideLen/2,y0=(1+2*j)*sideLen/2))
-#        clad_or_vec.append(openmc.ZCylinder(R=0.46,x0=(1+2*i)*sideLen/2,y0=(1+2*j)*sideLen/2))
-    
+fuel_or_vec = [ openmc.ZCylinder(R=0.39, x0=(1+2*i)*L/2, y0=(1+2*j)*L/2) \
+                for i in range(3) for j in range(3) ]
 
 
-
-fuel_region_vec = []
-#gap_region_vec = []
-#clad_region_vec = []
-
-for i in range(9):
-    fuel_region_vec.append(-fuel_or_vec[i])
-#    gap_region_vec.append(+fuel_or_vec[i] & -clad_ir_vec[i])
-#    clad_region_vec.append(+clad_ir_vec[i] & -clad_or_vec[i])
-    
-
-
-
-counter = 1
+"""
 fuel_vec = []
-#gap_vec  = []
-#clad_vec = []
 for i in range(9):
-    fuel = openmc.Cell(counter,'fuel'+str(i))
+    fuel = openmc.Cell(i,'fuel'+str(i))
     fuel.fill = uo2
-    fuel.region = fuel_region_vec[i]
-    counter += 1
-
-#    gap = openmc.Cell(counter,'gap'+str(i))
-#    gap.region = gap_region_vec[i]
-#    counter += 1
-    
-#    clad = openmc.Cell(counter,'clad'+str(i))
-#    clad.fill = zirconium
-#    clad.region = clad_region_vec[i]
-#    counter += 1
-    
+    fuel.region = -fuel_or_vec[i]
     fuel_vec.append(fuel)
-#    gap_vec.append(gap)    
-#    clad_vec.append(clad)
+"""
     
-    
+fuel_vec = [ openmc.Cell(i,'fuel'+str(i),fill=uo2,region=-fuel_or_vec[i]) for i in range(9)]
+
+
+left   = openmc.XPlane(x0=0.0,   boundary_type='reflective')
+right  = openmc.XPlane(x0=pitch, boundary_type='reflective')
+bottom = openmc.YPlane(y0=0.0,   boundary_type='reflective')
+top    = openmc.YPlane(y0=pitch, boundary_type='reflective')
 
 
 
 
-left = openmc.XPlane(x0=0.0, boundary_type='reflective')
-right = openmc.XPlane(x0=pitch, boundary_type='reflective')
-bottom = openmc.YPlane(y0=0.0, boundary_type='reflective')
-top = openmc.YPlane(y0=pitch, boundary_type='reflective')
+water_region = +left & -right & +bottom & -top &                       \
+               +fuel_or_vec[0] &  +fuel_or_vec[1] &  +fuel_or_vec[2] & \
+               +fuel_or_vec[3] &  +fuel_or_vec[4] &  +fuel_or_vec[5] & \
+               +fuel_or_vec[6] &  +fuel_or_vec[7] &  +fuel_or_vec[8] 
 
 
-
-
-
-
-
-
-water_region = +left & -right & +bottom & -top &                       \ 
-               +clad_or_vec[0] &  +clad_or_vec[1] &  +clad_or_vec[2] & \
-               +clad_or_vec[3] &  +clad_or_vec[4] &  +clad_or_vec[5] & \
-               +clad_or_vec[6] &  +clad_or_vec[7] &  +clad_or_vec[8] 
 
 moderator = openmc.Cell(50, 'moderator')
 moderator.fill = water
 moderator.region = water_region
 
 
-
-root = openmc.Universe(cells=(fuel_vec[0], gap_vec[0], clad_vec[0], moderator, \
-                              fuel_vec[1], gap_vec[1], clad_vec[1], \
-                              fuel_vec[2], gap_vec[2], clad_vec[2], \
-                              fuel_vec[3], gap_vec[3], clad_vec[3], \
-                              fuel_vec[4], gap_vec[4], clad_vec[4], \
-                              fuel_vec[5], gap_vec[5], clad_vec[5], \
-                              fuel_vec[6], gap_vec[6], clad_vec[6], \
-                              fuel_vec[7], gap_vec[7], clad_vec[7], \
-                              fuel_vec[8], gap_vec[8], clad_vec[8]))
+root = openmc.Universe(cells=(moderator,fuel_vec[0], fuel_vec[1], fuel_vec[2],\
+                             fuel_vec[3], fuel_vec[4], fuel_vec[5],           \
+                             fuel_vec[6], fuel_vec[7], fuel_vec[8] ))
 
 
 geom = openmc.Geometry(root)
@@ -132,7 +79,7 @@ geom.export_to_xml()
 settings = openmc.Settings()
 settings.batches = 100
 settings.inactive = 10
-settings.particles = 1000
+settings.particles = 10000
 settings.output = {'tallies': True}
 
 bounds = [0.0, 0.0, 0.0, pitch, pitch, pitch]
@@ -149,9 +96,9 @@ groups.group_edges = np.array([0., 0.625, 20.0e6])
 
 
 
-total_F = mgxs.TotalXS(domain=fuel, groups=groups)
-absorption_F = mgxs.AbsorptionXS(domain=fuel, groups=groups)
-scattering_F = mgxs.ScatterXS(domain=fuel, groups=groups)
+total_F = mgxs.TotalXS(domain=fuel_vec[0], groups=groups)
+absorption_F = mgxs.AbsorptionXS(domain=fuel_vec[0], groups=groups)
+scattering_F = mgxs.ScatterXS(domain=fuel_vec[0], groups=groups)
 
 total_M = mgxs.TotalXS(domain=moderator, groups=groups)
 absorption_M = mgxs.AbsorptionXS(domain=moderator, groups=groups)
@@ -160,7 +107,6 @@ scattering_M = mgxs.ScatterXS(domain=moderator, groups=groups)
 
 
 tallies_file = openmc.Tallies()
-
 tallies_file += total_F.tallies.values()
 tallies_file += absorption_F.tallies.values()
 tallies_file += scattering_F.tallies.values()
@@ -198,8 +144,9 @@ df
 
 
 ##################################################################
-# SETTINGS
+# PLOT
 ##################################################################
+"""
 p = openmc.Plot()
 p.filename = 'pinplot'
 p.width = (pitch, pitch)
@@ -219,5 +166,4 @@ openmc.plot_geometry()
 #from IPython.display import Image
 #Image("pinplot.png")
 
-
-
+"""
