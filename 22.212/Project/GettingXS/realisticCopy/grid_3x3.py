fc3a0fd15a7d19ca102a3649146ff96f0541db60
Amelia Trainer <ameliajo@mit.edu>
2018-11-20 14:37:22 -0500

    cleaning up grid_3x3

--- 22.212/Project/GettingXS/realisticCopy/grid_3x3.py
+++ 22.212/Project/GettingXS/realisticCopy/grid_3x3.py
@@ -6,13 +6,9 @@ import numpy as np
 
 
 radius_fuel = 0.3922
-radius_gap = 0.4001
-radius_clad = 0.4572
 pitch = 1.26
 
-
 # Basic materials
-
 uo2 = openmc.Material(name='fuel')
 uo2.add_element('U', 1, enrichment=3.2)
 uo2.add_element('O', 2)
@@ -25,12 +21,39 @@ water.add_element('H', 2.0)
 water.add_element('O', 1.0)
 water.set_density('g/cm3', 1.0)
 
+"""
+waters = []
+for i in range(9):
+    thiswater = openmc.Material(30+i, "h2o")
+    thiswater.add_element('H', 2.0)
+    thiswater.add_element('O', 1.0)
+    thiswater.set_density('g/cm3', 1.0)
+    waters.append(thiswater)
+
+uo2s = []
+for i in range(9):
+    thisuo2 = openmc.Material(name='fuel')
+    thisuo2.add_element('U', 1, enrichment=3.2)
+    thisuo2.add_element('O', 2)
+    #thisuo2.add_element('Gd', 0.0007)
+    thisuo2.set_density('g/cc', 10.341)
+    uo2s.append(thisuo2)
+
+materials = openmc.Materials(uo2s+waters)
+"""
+
+
+
+
+
 
 materials = openmc.Materials([uo2, water])
 materials.export_to_xml()
 
 
 L = pitch
+
+fCylinders = [ openmc.ZCylinder(R=radius_fuel, x0=0.5*L+i*L, y0=0.5*L+j*L) for j in range(3) for i in range(3) ]
 
 x1 = openmc.XPlane(x0=0.0, boundary_type='reflective')
 x2 = openmc.XPlane(x0=1*L)
@@ -42,34 +65,35 @@ y2 = openmc.YPlane(y0=1*L)
 y3 = openmc.YPlane(y0=2*L)
 y4 = openmc.YPlane(y0=3*L, boundary_type='reflective')
 
+xP = [x1,x2,x3,x4]
+yP = [y1,y2,y3,y4]
 
 
 
+waterReg = +x1 & -x4 & +y1 & -y4 &                              \
+           +fCylinders[0] &  +fCylinders[1] &  +fCylinders[2] & \
+           +fCylinders[3] &  +fCylinders[4] &  +fCylinders[5] & \
+           +fCylinders[6] &  +fCylinders[7] &  +fCylinders[8] 
 
-rf0 = openmc.ZCylinder(R=radius_fuel,x0=pitch*0.5,y0=pitch*0.5)
-rf1 = openmc.ZCylinder(R=radius_fuel,x0=pitch*0.5,y0=pitch*1.5)
-rf2 = openmc.ZCylinder(R=radius_fuel,x0=pitch*0.5,y0=pitch*2.5)
-rf3 = openmc.ZCylinder(R=radius_fuel,x0=pitch*1.5,y0=pitch*0.5)
-rf4 = openmc.ZCylinder(R=radius_fuel,x0=pitch*1.5,y0=pitch*1.5)
-rf5 = openmc.ZCylinder(R=radius_fuel,x0=pitch*1.5,y0=pitch*2.5)
-rf6 = openmc.ZCylinder(R=radius_fuel,x0=pitch*2.5,y0=pitch*0.5)
-rf7 = openmc.ZCylinder(R=radius_fuel,x0=pitch*2.5,y0=pitch*1.5)
-rf8 = openmc.ZCylinder(R=radius_fuel,x0=pitch*2.5,y0=pitch*2.5)
+fCells = [openmc.Cell(name='fuel'+str(i), fill=uo2, region=-fCylinders[i]) for i in range(9)]
+#fCells = [openmc.Cell(name='fuel'+str(i), fill=uo2s[i], region=-fCylinders[i]) for i in range(9)]
 
-waterReg = +rf0 & +rf1 & +rf2 & +rf3 & +rf4 & +rf5 & +rf6 & +rf7 & +rf8 & +x1 & -x4 & +y1 & -y4
+mCells = []
+count = 0
+for j in range(3):
+    for i in range(3):
+        mRegion = waterReg & +xP[i] & -xP[i+1] & +yP[j] & -yP[j+1]
+        mCells.append(openmc.Cell(70+count,'mod'+str(count),fill=water,region=mRegion))
+        #mCells.append(openmc.Cell(70+count,'mod'+str(count),fill=waters[count],region=mRegion))
+        count += 1
 
-fuel0 = openmc.Cell(name='fuel0', fill=uo2, region=-rf0)
-fuel1 = openmc.Cell(name='fuel1', fill=uo2, region=-rf1)
-fuel2 = openmc.Cell(name='fuel2', fill=uo2, region=-rf2)
-fuel3 = openmc.Cell(name='fuel3', fill=uo2, region=-rf3)
-fuel4 = openmc.Cell(name='fuel4', fill=uo2, region=-rf4)
-fuel5 = openmc.Cell(name='fuel5', fill=uo2, region=-rf5)
-fuel6 = openmc.Cell(name='fuel6', fill=uo2, region=-rf6)
-fuel7 = openmc.Cell(name='fuel7', fill=uo2, region=-rf7)
-fuel8 = openmc.Cell(name='fuel8', fill=uo2, region=-rf8)
 
-mod = openmc.Cell(name='moderator', fill=water, region=waterReg)
-root = openmc.Universe(cells=(fuel0,fuel1,fuel2,fuel3,fuel4,fuel5,fuel6,fuel7,fuel8,mod))
+
+root = openmc.Universe(cells=(                                              \
+    mCells[0], mCells[1], mCells[2], mCells[3], mCells[4], mCells[5], \
+    mCells[6], mCells[7], mCells[8], fCells[0], fCells[1], fCells[2], \
+    fCells[3], fCells[4], fCells[5], fCells[6], fCells[7], fCells[8] ))
+
 geometry = openmc.Geometry(root)
 geometry.export_to_xml()
 
@@ -77,100 +101,51 @@ geometry.export_to_xml()
 # Settings
 
 settings = openmc.Settings()
-
 settings.batches = 100
 settings.inactive = 25
 settings.particles = 5000
 
 
-# Set the initial source to a flat distribution born only in the fuel.
-space = openmc.stats.Box((0.0, 0.0, 0.0),
-     (3*pitch, 3*pitch, 0))
+space = openmc.stats.Box((0.0, 0.0, 0.0),(3.0*pitch, 3.0*pitch, 0))
 settings.source = openmc.Source(space=space)
 settings.export_to_xml()
 
-
-
-
-# # MGXS Tallies
 groups = openmc.mgxs.EnergyGroups([0.0,0.625,20e6])
 
-# Instantiate an MGXS library.
 mgxs_lib = openmc.mgxs.Library(geometry)
 mgxs_lib.energy_groups = groups
 
-# Don't apply any anisotropic scattering corrections.
 mgxs_lib.correction = 'P0'
 
-# Set the desired MGXS data.
 mgxs_lib.mgxs_types = ('total','nu-transport','absorption','nu-fission', 'fission','consistent nu-scatter matrix','chi')
 
-# Define the domain and build the library.
 mgxs_lib.domain_type = 'cell'
 mgxs_lib.domains = geometry.get_all_material_cells().values()
-#mgxs_lib.domain_type = 'universe'
-#mgxs_lib.domains = geometry.get_all_material_universes().values()
 mgxs_lib.build_library()
 
 
-# Add the tallies.
+
 tallies = openmc.Tallies()
 mgxs_lib.add_to_tallies_file(tallies)
-
 tallies.export_to_xml()
+
 
 openmc.run()
 
 
-# Load the statepoint and the MGXS results.
 sp = openmc.StatePoint('statepoint.100.h5')
 mgxs_lib.load_from_statepoint(sp)
 
+for i in range(9): mgxs_lib.domains[i].name   = 'fuel' + str(i)
+for i in range(9): mgxs_lib.domains[9+i].name = 'mod'  + str(i)
 
-# Pick out the fuel and moderator domains.
-#fuel0 = mgxs_lib.domains[0]
-fuel0 = mgxs_lib.domains[0]
-fuel0.name = 'fuel0'
-fuel1 = mgxs_lib.domains[1]
-fuel1.name = 'fuel1'
-fuel2 = mgxs_lib.domains[2]
-fuel2.name = 'fuel2'
-fuel3 = mgxs_lib.domains[3]
-fuel3.name = 'fuel3'
-fuel4 = mgxs_lib.domains[4]
-fuel4.name = 'fuel4'
-fuel5 = mgxs_lib.domains[5]
-fuel5.name = 'fuel5'
-fuel6 = mgxs_lib.domains[6]
-fuel6.name = 'fuel6'
-fuel7 = mgxs_lib.domains[7]
-fuel7.name = 'fuel7'
-fuel8 = mgxs_lib.domains[8]
-fuel8.name = 'fuel8'
-
-
-moderator = mgxs_lib.domains[9]
-moderator.name = 'moderator'
-#assert moderator.name == 'moderator'
-
-
-mgxs_file = mgxs_lib.create_mg_library(xs_type='macro',xsdata_names=['fuel0','fuel1','fuel2','fuel3','fuel4','fuel5','fuel6','fuel7','fuel8','moderator'])
-#mgxs_file = mgxs_lib.create_mg_library(xs_type='macro',xsdata_names=['fuel0'])
+mgxs_file = mgxs_lib.create_mg_library(xs_type='macro',xsdata_names=['fuel0','fuel1','fuel2','fuel3','fuel4','fuel5','fuel6','fuel7','fuel8','mod0','mod1','mod2','mod3','mod4','mod5','mod6','mod7','mod8'])
 
 
 mgxs_file.export_to_hdf5()
 
-
-f0Data = mgxs_file.get_by_name('fuel0')
-f1Data = mgxs_file.get_by_name('fuel1')
-f2Data = mgxs_file.get_by_name('fuel2')
-f3Data = mgxs_file.get_by_name('fuel3')
-f4Data = mgxs_file.get_by_name('fuel4')
-f5Data = mgxs_file.get_by_name('fuel5')
-f6Data = mgxs_file.get_by_name('fuel6')
-f7Data = mgxs_file.get_by_name('fuel7')
-f8Data = mgxs_file.get_by_name('fuel8')
-mData = mgxs_file.get_by_name('moderator')
+fDatas = [ mgxs_file.get_by_name('fuel'+str(i)) for i in range(9) ]
+mDatas = [ mgxs_file.get_by_name('mod' +str(i)) for i in range(9) ]
 
 """
 print(fData.total)
@@ -182,51 +157,21 @@ print(fData.scatter_matrix)
 print(fData.nu_fission)
 """
 
-fDatas = [f0Data,f1Data,f2Data,f3Data,f4Data,f5Data,f6Data,f7Data,f8Data]
 for fData in fDatas:
     assert(abs(fData.total[0][0]-                \
               (fData.absorption[0][0]+           \
                fData.scatter_matrix[0][0][0][0]+ \
                fData.scatter_matrix[0][0][1][0])) < 1e-12 )
+for mData in mDatas:
+    assert(abs(mData.total[0][0]-                \
+              (mData.absorption[0][0]+           \
+               mData.scatter_matrix[0][0][0][0]+ \
+               mData.scatter_matrix[0][0][1][0])) < 1e-12 )
 
 
 
-"""
-print(f0Data.total[0][0])
-print((f0Data.absorption[0][0]+           \
-       f0Data.scatter_matrix[0][0][0][0]+ \
-       f0Data.scatter_matrix[0][0][1][0]))
 
-assert(abs(f0Data.total[0][0]-                \
-          (f0Data.absorption[0][0]+           \
-           f0Data.scatter_matrix[0][0][0][0]+ \
-           f0Data.scatter_matrix[0][0][1][0])) < 1e-12 )
-
-
-print(f1Data.total[0][0])
-print((f1Data.absorption[0][0]+           \
-       f1Data.scatter_matrix[0][0][0][0]+ \
-       f1Data.scatter_matrix[0][0][1][0]))
-
-assert(abs(f1Data.total[0][0]-                \
-          (f1Data.absorption[0][0]+           \
-           f1Data.scatter_matrix[0][0][0][0]+ \
-           f1Data.scatter_matrix[0][0][1][0])) < 1e-12 )
-
-
-
-print(mData.total[0][0])
-print((mData.absorption[0][0]+           \
-       mData.scatter_matrix[0][0][0][0]+ \
-       mData.scatter_matrix[0][0][1][0]))
-
-assert(abs(mData.total[0][0]-                \
-          (mData.absorption[0][0]+           \
-           mData.scatter_matrix[0][0][0][0]+ \
-           mData.scatter_matrix[0][0][1][0])) < 1e-12 )
-
-print(mData.scatter_matrix)
-"""
+print(mDatas[0].scatter_matrix)
 
 ##################################################################
 # PLOT
