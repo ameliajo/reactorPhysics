89fcb93031af09a12c335c87aedb0a69249e6274
Amelia Trainer <ameliajo@mit.edu>
2020-02-26 12:43:40 -0500

    cleaned up some plots

--- 22.211/pset2/freeGas/alpha_PDF.py
+++ 22.211/pset2/freeGas/alpha_PDF.py
@@ -2,33 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import pi, exp
 
-
-
-
 def getAlphaRange(beta,A,kbT,E,nAlpha):
     aMin = ( (E)**0.5 - (E+beta*kbT)**0.5 )**2 / (A*kbT)
     aMax = ( (E)**0.5 + (E+beta*kbT)**0.5 )**2 / (A*kbT)
     return np.linspace(aMin,aMax,nAlpha)
 
-
 def getFreeGasSAB(alpha,beta):
     return (4.0*pi*alpha)**-0.5 * exp( -(alpha+beta)**2 / (4.0*alpha) )
 
-
-
-
-E = 1.0
-kb = 8.617e-5
-kbT = kb * 296.0
-A = 1.0
+E    = 1.0
+A    = 1.0
+kbT  = 8.617e-5 * 296.0
 beta = 10.0
 
-def getAlphaPDF(beta,kbT,A,E):
-    alphaRange = getAlphaRange(beta,A,kbT,E,50000)
+def getAlphaPDF(beta,kbT,A,E,numAlpha=50000):
+    alphaRange = getAlphaRange(beta,A,kbT,E,numAlpha)
     sabChunk = [ getFreeGasSAB(alpha,beta) for alpha in alphaRange ]
-    denominator = np.trapz(sabChunk,alphaRange)
-    eq15 = [sabChunk[a]/denominator for a in range(len(sabChunk))]
-    print(np.trapz(eq15,alphaRange))
+    scaling = 1.0/np.trapz(sabChunk,alphaRange)
+    eq15 = [sabChunk[a]*scaling for a in range(len(sabChunk))]
     return alphaRange, eq15
 
 def getAlphaCDF(alphaRange, alphaPDF):
@@ -38,15 +29,15 @@ def getAlphaCDF(alphaRange, alphaPDF):
     invTotal = 1.0/alphaCDF[len(alphaCDF)-1]
     return [CDF*invTotal for CDF in alphaCDF]
 
-
 alphaRange, alphaPDF = getAlphaPDF(beta,kbT,A,E)
+plt.xlabel('alpha')
+plt.title('H in H2O Free Gas S(a,b) alpha PDF (296 K, beta=10)'); 
+plt.plot(alphaRange,alphaPDF,label='alpha PDF'); plt.show()
 alphaCDF = getAlphaCDF(alphaRange,alphaPDF)
-plt.title('H in H2O Free Gas S(a,b) alpha PDF and CDF')
-plt.plot(alphaRange,alphaPDF,label='alpha PDF')
-plt.plot(alphaRange,alphaCDF,label='alpha CDF')
+plt.xlabel('alpha')
+plt.title('H in H2O Free Gas S(a,b) alpha CDF (296 K, beta=10)'); 
+plt.plot(alphaRange,alphaCDF,label='alpha CDF'); plt.show()
 
-plt.legend(loc='best')
-plt.show()
 
  
 
--- 22.211/pset2/freeGas/beta_PDF_CDF.py
+++ 22.211/pset2/freeGas/beta_PDF_CDF.py
@@ -2,64 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import pi, exp
 
-
-
-
 def getAlphaRange(beta,A,kbT,E,nAlpha):
     aMin = ( (E)**0.5 - (E+beta*kbT)**0.5 )**2 / (A*kbT)
     aMax = ( (E)**0.5 + (E+beta*kbT)**0.5 )**2 / (A*kbT)
     return np.linspace(aMin,aMax,nAlpha)
 
-
 def getFreeGasSAB(alpha,beta):
     return (4.0*pi*alpha)**-0.5 * exp( -(alpha+beta)**2 / (4.0*alpha) )
 
+def getBetaPDF(A,kbT,E):
+    betaMin, betaMax = -E/kbT, 20.0
+    betas = list(np.linspace(betaMin,betaMax,1000)) 
+    eq14 = [0.0]*len(betas)
+    for b,beta in enumerate(betas):
+        alphaRange = getAlphaRange(beta,A,kbT,E,2000)
+        sabChunk = [ getFreeGasSAB(alpha,beta) for alpha in alphaRange ]
+        eq14[b] = np.trapz(sabChunk,alphaRange)
+    eq14Denominator = np.trapz(eq14,betas)
+    eq14 = [PDFVal/eq14Denominator for PDFVal in eq14]
+    return betas, eq14
 
-
-
-def getAlphaPDF(beta,kbT,A,E):
-    alphaRange = getAlphaRange(beta,A,kbT,E,50000)
-    sabChunk = [ getFreeGasSAB(alpha,beta) for alpha in alphaRange ]
-    denominator = np.trapz(sabChunk,alphaRange)
-    eq15 = [sabChunk[a]/denominator for a in range(len(sabChunk))]
-    print(np.trapz(eq15,alphaRange))
-    return alphaRange, eq15
-
-def getAlphaCDF(alphaRange, alphaPDF):
-    alphaCDF = [0.0]*len(alphaRange)
-    for a in range(1,len(alphaRange)):
-        alphaCDF[a] = alphaCDF[a-1]+alphaPDF[a]
-    invTotal = 1.0/alphaCDF[len(alphaCDF)-1]
-    return [CDF*invTotal for CDF in alphaCDF]
-
-
+def getBetaCDF(betas,betaPDF):
+    betaCDF = [0.0]*len(betaPDF)
+    for b in range(1,len(betaPDF)):
+        betaCDF[b] = betaCDF[b-1]+betaPDF[b]
+    return [x/betaCDF[-1] for x in betaCDF]
 
 E = 1.0
-kb = 8.617e-5
-kbT = kb * 296.0
 A = 1.0
+kbT = 8.617e-5 * 296.0
 
-betaMin = -E/kbT
-betaMax = 20.0
+betas, betaPDF = getBetaPDF(A,kbT,E)
+plt.title('H in H2O Free Gas S(a,b) beta PDF (296 K)'); 
+plt.plot(betas,betaPDF)
+plt.show()
 
-betas = list(np.linspace(betaMin,betaMax,1000)) 
-betas.sort()
-
-eq14 = [0.0]*len(betas)
-for b,beta in enumerate(betas):
-    alphaRange = getAlphaRange(beta,A,kbT,E,2000)
-    sabChunk = [ getFreeGasSAB(alpha,beta) for alpha in alphaRange ]
-    eq14[b] = np.trapz(sabChunk,alphaRange)
-
-    
-eq14Denominator = np.trapz(eq14,betas)
-eq14 = [PDFVal/eq14Denominator for PDFVal in eq14]
-
-cdf = [0.0]*len(eq14)
-for b in range(1,len(eq14)):
-    cdf[b] = cdf[b-1]+eq14[b]
-cdf = [x/cdf[len(cdf)-1] for x in cdf]
-plt.plot(betas,eq14)
-plt.plot(betas,cdf)
+betaCDF = getBetaCDF(betas,betaPDF)
+plt.title('H in H2O Free Gas S(a,b) beta CDF (296 K)'); 
+plt.plot(betas,betaCDF)
 plt.show()
 
